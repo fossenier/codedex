d1cbadb2f5eb6a89d517d54a513f83ed0855fc23
Logan Fossenier <logan.fossenier@gmail.com>
2024-07-12 08:45:13 -0600

    chore: size up the material on the home page

--- codedex/app/page.tsx
+++ codedex/app/page.tsx
@@ -4,16 +4,17 @@ import Image from 'next/image'; // Importing the Image component from next/image
 const HomePage = () => {
   return (
     <div className="hero bg-base-200 min-h-screen relative flex flex-col justify-start items-center">
-      <div className="max-w-md text-center z-10 relative p-20">
+      <div className="text-center z-10 relative p-20">
         <Image
           src="/sipnplay.png"
           alt="Sip & Play Image"
-          width={800}
-          height={800}
+          className="w-96 md:w-128"
+          width={1024}
+          height={1024}
         />
       </div>
       <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-1/2 h-1/2 bg-orange-300 rounded-t-full flex justify-center items-center z-0">
-        <span className="text-white text-4xl font-bold">🧁</span>
+        <span className="text-white text-9xl font-bold">🧁</span>
       </div>
     </div>
   );
