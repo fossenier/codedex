7f1a73fc7e4d8050d7694accf03ae6f2e1645923
Logan Fossenier <logan.fossenier@gmail.com>
2024-07-11 19:50:18 -0600

    feat: logo

--- codedex/app/page.tsx
+++ codedex/app/page.tsx
@@ -1,14 +1,15 @@
+import Image from 'next/image';
+
 const HomePage = () => {
-  // const [mounted, setMounted] = useState(false);
-
-  // useEffect(() => {
-  //   setMounted(true);
-  // }, []);
-
   return (
     <div className="hero bg-base-200 min-h-screen relative flex flex-col justify-start items-center">
       <div className="max-w-md text-center z-10 relative p-20">
-        <h1 className="text-5xl font-bold">Sip & Play</h1>
+        <Image
+        src="/sipnplay.png"
+        alt="Sip & Play Image"
+        width={800}
+        height={800}
+      />
       </div>
       <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-1/2 h-1/2 bg-orange-300 rounded-t-full flex justify-center items-center z-0">
         <span className="text-white text-4xl font-bold">🧁</span>
